90619355bb7b7c0f3b64f131138d7589ce164a38
HarishSai-sketch <harishsai0804@gmail.com>
2025-05-24 18:43:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Typing animation
+document.getElementById("typewriter").textContent = "B Harish Sai";
+
+// Project details popup
+function showProjectDetails(projectName) {
+  alert("Details about " + projectName + " coming soon!");
+}
+
+// Contact form submission
+function submitForm(event) {
+  event.preventDefault();
+  alert("Thanks for contacting me!");
+}
